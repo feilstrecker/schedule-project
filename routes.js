e798669307d498090c62ba354b6efdc47adab635
leozinga <zleho70@gmail.com>
2023-03-25 22:49:54 -0300

    new visual

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
 const contatoController = require('./src/controllers/contatoController');
+const registerController = require('./src/controllers/registerController');
 const middlewares = require('./src/middlewares/middleware')
 
 // Rota da home
@@ -10,9 +11,12 @@ route.get('/', homeController.index);
 
 // rotas de login
 route.get('/login/index', loginController.index);
-route.post('/login/register', loginController.register);
 route.post('/login/login', loginController.login);
 route.get('/login/logout', loginController.logout);
+
+// rotas de registro
+route.get('/register/index', registerController.index);
+route.post('/register/register', registerController.register);
 
 // rotas de contato
 route.get('/contato/index', middlewares.userLogged, contatoController.index);
--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -4,32 +4,6 @@ exports.index = (req, res) => {
     res.render('login');
   };
 
-exports.register = async function(req, res) {
-  try {
-    const login = new Login(req.body)
-    await login.register();
-
-    const erros = login.errors;
-    const success = login.success;
-    if(erros.length > 0) {
-      req.flash('errors', erros);
-      req.session.save(function() {
-        return res.redirect('index');
-      })
-    } else {
-      req.flash('success', success)
-      req.session.save(function() {
-        return res.redirect('index');
-      })
-    }
-    return;
-
-  } catch(er){
-    console.log(er);
-    res.render('404');
-  }
-}
-
 exports.login = async function(req, res) {
   try {
     const login = new Login(req.body)
